5299d056c8fe1d8634b6ec6f56559c79a48cc507
lavya30 <lavayagoel2006@gmail.com>
2026-03-03 18:23:53 +0530

    add main.js

---
+++ electron/main.js
@@ -0,0 +1,21 @@
+const { app, BrowserWindow } = require('electron')
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  })
+
+  win.loadURL('http://localhost:3000')
+  win.webContents.openDevTools()
+}
+
+app.whenReady().then(createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
